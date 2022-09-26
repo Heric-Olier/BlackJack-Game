a3d6141ba96471cc43bb19dce1cb3667edc448a6
Heric Olier <hericolier.code@gmail.com>
2022-09-26 17:10:08 -0500

    add SweetAlerts Library

---
+++ assets/js/alerts.js
@@ -0,0 +1,13 @@
+    const alertMessage = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+
+    export { alertMessage }
