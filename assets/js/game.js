7cbf281a8cb78ce4f6b1f7fb5ea9a7b04682af35
Heric Olier <hericolier.code@gmail.com>
2022-09-26 20:05:41 -0500

    add Card Game Board Section & Improve JS code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,43 +1,13 @@
-import { alertMessage } from "./alerts.js";
+import { selectBet, startGame } from "./start-game.js";
 
-const chip10 = document.getElementById("chip-10");
-const chip25 = document.getElementById("chip-25");
-const chip50 = document.getElementById("chip-50");
-const chip100 = document.getElementById("chip-100");
-const betamount = document.querySelector("[data-bet-amount]");
-const btnStartGame = document.getElementById("btn-start-game");
 
-const bet10 = () => {
-  betamount.innerHTML = chip10.dataset.value;
-};
 
-const bet25 = () => {
-  betamount.innerHTML = chip25.dataset.value;
-};
 
-const bet50 = () => {
-  betamount.innerHTML = chip50.dataset.value;
-};
 
-const bet100 = () => {
-  betamount.innerHTML = chip100.dataset.value;
-};
 
-const startGame = () => {
-  if (betamount.innerHTML == 0) {
-    alertMessage.fire({
-      icon: "error",
-      title: "Please select a bet amount!",
-    });
-  } else {
-    alert("Game started");
-  }
-};
 
-btnStartGame.addEventListener("click", startGame);
 
-chip10.addEventListener("click", bet10);
-chip25.addEventListener("click", bet25);
-chip50.addEventListener("click", bet50);
-chip100.addEventListener("click", bet100);
+
+
+
 
---
+++ assets/js/start-game.js
@@ -0,0 +1,37 @@
+import { alertMessage } from "./alerts.js";
+
+const chip10 = document.getElementById("chip-10");
+const chip25 = document.getElementById("chip-25");
+const chip50 = document.getElementById("chip-50");
+const chip100 = document.getElementById("chip-100");
+const betamount = document.querySelector("[data-bet-amount]");
+const cardGameHome = document.getElementById("card__game-home");
+const cardGameBoard = document.getElementById("card__game-board");
+const btnStartGame = document.getElementById("btn-start-game");
+
+// funcion para seleccionar la apuesta
+const selectBet = (chip) => {
+  betamount.innerHTML = chip.dataset.value;
+};
+
+chip10.addEventListener("click", () => selectBet(chip10));
+chip25.addEventListener("click", () => selectBet(chip25));
+chip50.addEventListener("click", () => selectBet(chip50));
+chip100.addEventListener("click", () => selectBet(chip100));
+
+// funcion para iniciar el juego
+const startGame = () => {
+  if (betamount.innerHTML == 0) {
+    alertMessage.fire({
+      icon: "error",
+      title: "Please select a bet amount!",
+    });
+  } else {
+    cardGameHome.classList.add("d-none");
+    cardGameBoard.classList.remove("d-none");
+  }
+};
+
+btnStartGame.addEventListener("click", startGame);
+
+export { selectBet, startGame };
